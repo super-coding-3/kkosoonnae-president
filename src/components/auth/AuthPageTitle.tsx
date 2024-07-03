106d74ca7ec12f75c9a273102f2811f4ca3a5943
yujin Park <101633842+ruby-yujin@users.noreply.github.com>
2024-07-03 18:36:36 +0900

    [feature] auth페이지 공통타이틀

---
+++ src/components/auth/AuthPageTitle.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface AuthPageTitleProps {
+  subtitle: string;
+  title: string;
+}
+
+const AuthPageTitle: React.FC<AuthPageTitleProps> = ({ subtitle, title }) => {
+  return (
+    <div className="text-center">
+      <img
+        src="img/logo/logo.svg"
+        alt="로고"
+        className="block w-24 mx-auto -mt-40 mb-12"
+      />
+      <p className="text-black text-xl mb-2">{subtitle}</p>
+      <h1 className="text-black text-3xl">{title}</h1>
+    </div>
+  );
+};
+
+export default AuthPageTitle;
